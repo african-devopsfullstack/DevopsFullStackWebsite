f5da2265ae46fec8d03e5eef6759d46ae25d9952
Idris Fagbemi <idrisniyi94@gmail.com>
2023-09-17 11:04:52 -0500

    Update code and added sonar scanner

--- database.py
+++ database.py
@@ -5,16 +5,21 @@ import os
 
 load_dotenv()
 
+try:
+    config = {
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
+        'database': os.getenv('DB_NAME')
+    }
 
-config = {
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
-    'host': os.getenv('DB_HOST'),
-    'database': os.getenv('DB_NAME')
-}
 
-
-conn = mysql.connect(**config)
+    conn = mysql.connect(**config)
+    if conn.is_connected():
+        print("Connected to MySQL database")
+except Error as e:
+    print(e)
+    pass
 
 def get_connection():
     print("Opened database successfully")
@@ -22,7 +27,10 @@ def get_connection():
     
 def create_table():
     cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, fname VARCHAR(255) NOT NULL, lname VARCHAR(255) NOT NULL, gender VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, phone_number VARCHAR(255), primary key(id))')
+    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, fname VARCHAR(255) NOT NULL, lname VARCHAR(255) NOT NULL, \
+                   gender VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, \
+                   phone_number VARCHAR(255), linkedin_profile VARCHAR(255), github_profile VARCHAR(255), about VARCHAR(255), \
+                   user_location VARCHAR(255), working_at VARCHAR(255), primary key(id))')
     print("Table created successfully")
     conn.close()
 
@@ -41,3 +49,47 @@ def get_user(email):
     for row in result:
         return row
     conn.close()
+
+def get_all_users(id: int):
+    try:
+        query = f"SELECT * FROM users WHERE id not in ({id})"
+        cursor = conn.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print(e)
+        return None
+
+def get_all_posts():
+    try:
+        query = f"""SELECT
+        users.fname as first_name,
+        users.lname as last_name,
+        posts.post_content AS post_content,
+        (
+            SELECT TIMESTAMPDIFF(HOUR, posts.created_at, NOW())
+            FROM posts
+            WHERE posts.user_id = users.id
+        ) AS time_elapsed_seconds
+    FROM
+        users
+    INNER JOIN
+        posts ON users.id = posts.user_id;"""
+        cursor = conn.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+        
+    except Exception as e:
+        print(e)
+        conn.close()
+        return None
+    # for row in result:
+    #     return row
+
+# create_table()
+
+# print(get_all_posts())
+
+# print(get_all_users(0))
